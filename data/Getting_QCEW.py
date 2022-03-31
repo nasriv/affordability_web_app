1f789e3589f3070a64350df4b4db06aad52ac142
Nick Orsini <61241003+Norsini598@users.noreply.github.com>
2022-03-30 22:42:22 -0400

    added code to get "meta" data about qcew- improvements to Getting_QCEW code

--- data/Getting_QCEW.py
+++ data/Getting_QCEW.py
@@ -1,17 +1,41 @@
 import urllib.request
 import urllib
+import pandas as pd
+import pathlib
+import os
 
+thisfile = pathlib.Path(__file__).parent.resolve()
+print(thisfile)
 # *******************************************************************************
-# qcewCreateDataRows : This function takes a raw csv string and splits it into
+# Get_qcew_data : This function takes a raw csv string and splits it into
 # a two-dimensional array containing the data and the header row of the csv file
 # a try/except block is used to handle for both binary and char encoding
-def qcewCreateDataRows(csv):
+def Get_qcew_data(csv):
     dataRows = []
-    try: dataLines = csv.decode().split('\r\n')
-    except er: dataLines = csv.split('\r\n');
+    try: 
+        dataLines = csv.decode().split('\r\n')
+    except: 
+        dataLines = csv.split('\r\n')
+    
     for row in dataLines:
-        dataRows.append(row.split(','))
-    return dataRows
+        dataRows.append(row.replace("\"", "").split(','))
+    
+    df = pd.DataFrame(data = dataRows[1:], columns = dataRows[0]).dropna()
+    
+    #using the QCEW Field Layouts we've determined which fields are numeric
+    numeric_cols = ["qtrly_estabs", "month1_emplvl", "month2_emplvl", "month3_emplvl",
+                    "total_qtrly_wages", "taxable_qtrly_wages", "qtrly_contributions",
+                    "avg_wkly_wage", "lq_qtrly_estabs", "lq_month1_emplvl", 
+                    "lq_month2_emplvl", "lq_month3_emplvl", "lq_total_qtrly_wages",
+                    "lq_taxable_qtrly_wages", "lq_qtrly_contributions", "lq_avg_wkly_wage", 
+                    "oty_qtrly_estabs_chg", "oty_qtrly_estabs_pct_chg", "oty_month1_emplvl_chg",
+                    "oty_month1_emplvl_pct_chg", "oty_month2_emplvl_chg", "oty_month2_emplvl_pct_chg", 
+                    "oty_month3_emplvl_chg", "oty_month3_emplvl_pct_chg", "oty_total_qtrly_wages_chg", 
+                    "oty_total_qtrly_wages_pct_chg", "oty_taxable_qtrly_wages_chg", 
+                    "oty_taxable_qtrly_wages_pct_chg", "oty_qtrly_contributions_chg", 
+                    "oty_qtrly_contributions_pct_chg", "oty_avg_wkly_wage_chg", "oty_avg_wkly_wage_pct_chg"]
+    df[numeric_cols] = df[numeric_cols].astype(float)
+    return df
 # *******************************************************************************
 
 # *******************************************************************************
@@ -21,12 +45,12 @@ def qcewCreateDataRows(csv):
 # For all area codes and titles see:
 # http://www.bls.gov/cew/doc/titles/area/area_titles.htm
 #
-def qcewGetAreaData(year,qtr,area):
-    urlPath = f"http://data.bls.gov/cew/data/api/{year}/{qtr.lower()}/area/{area.upper()}.csv"
+def qcewGetAreaData(year, qtr, area):
+    urlPath = f"http://data.bls.gov/cew/data/api/{year}/{qtr}/area/{area.upper()}.csv"
     httpStream = urllib.request.urlopen(urlPath)
     csv = httpStream.read()
     httpStream.close()
-    return qcewCreateDataRows(csv)
+    return Get_qcew_data(csv)
 # *******************************************************************************
 
 # *******************************************************************************
@@ -37,12 +61,12 @@ def qcewGetAreaData(year,qtr,area):
 # For all industry codes and titles see:
 # http://www.bls.gov/cew/doc/titles/industry/industry_titles.htm
 #
-def qcewGetIndustryData(year,qtr,industry):
-    urlPath = f"http://data.bls.gov/cew/data/api/{year}/{qtr.lower()}/{industry/{IND}.csv"
+def qcewGetIndustryData(year, qtr, industry):
+    urlPath = f"http://data.bls.gov/cew/data/api/{year}/{qtr}/industry/{industry}.csv"
     httpStream = urllib.request.urlopen(urlPath)
     csv = httpStream.read()
     httpStream.close()
-    return qcewCreateDataRows(csv)
+    return Get_qcew_data(csv)
 # *******************************************************************************
 
 # *******************************************************************************
@@ -57,22 +81,24 @@ def qcewGetSizeData(year,size):
     httpStream = urllib.request.urlopen(urlPath)
     csv = httpStream.read()
     httpStream.close()
-    return qcewCreateDataRows(csv)
+    return Get_qcew_data(csv)
 # *******************************************************************************
 
-
+def Download_InformativeFiles():
+    layout_url = "https://www.bls.gov/cew/about-data/downloadable-file-layouts/quarterly/naics-based-quarterly-layout-csv.csv"
+    size_url = "https://www.bls.gov/cew/classifications/size/size-titles-csv.csv"
+    industries_url = "https://www.bls.gov/cew/classifications/industry/industry-titles-csv.csv"
+    aggregations_url = "https://www.bls.gov/cew/classifications/aggregation/agg-level-titles-csv.csv"
+    
+    files = [(layout_url, 'layout.csv'), (size_url, 'size.csv'), (industries_url, 'industries.csv'), (aggregations_url, 'aggregations.csv')]
+    for url, filename in files:
+        response = urllib.request.urlopen(url)  
+        file = open(os.path.join(thisfile, filename), 'wb')
+        file.write(response.read())
+        file.close()
+        
 if __name__ == '__main__':
-	Maryland_Data = qcewGetAreaData("2015","1","24000")
-	Auto_Manufacturing = qcewGetIndustryData("2015","1","3361")
-	SizeData = qcewGetSizeData("2015","6")
-
-	# prints the industry_code in row 5.
-	print(Maryland_Data[5][2])
-
-
-	# prints the area_fips in row 1.
-	print(Auto_Manufacturing[1][0])
-
-
-	# prints the own_code in row 1.
-	print(SizeData[1][1])
+    Download_InformativeFiles()
+    Maryland_Data = qcewGetAreaData(2015,1, "24000")
+    Auto_Manufacturing = qcewGetIndustryData(2015, 1, "3361")
+    SizeData = qcewGetSizeData(2015, 6)
